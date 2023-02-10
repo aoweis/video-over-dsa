2a0cc6bb688d4118bc99acd3fd30565c3116ccc9
Ahmad Oweis <oweis@ieee.org>
2023-02-10 08:57:03 +0300

    updated transmitter.grc

---
+++ Transmitter/transmitter.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: ahmad
+# GNU Radio version: 3.10.5.0
+
+from packaging.version import Version as StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import blocks
+import pmt
+from gnuradio import digital
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import bladeRF
+import time
+
+
+
+from gnuradio import qtgui
+
+class transmitter(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "transmitter")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.num_key = num_key = "packet_num"
+        self.len_key = len_key = "packet_len"
+        self.tx_gain = tx_gain = 1
+        self.samp_rate = samp_rate = 1e6
+        self.hdr_format = hdr_format = digital.header_format_crc(len_key, num_key)
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True, #plotconst
+            None # parent
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.digital_protocol_formatter_bb_0 = digital.protocol_formatter_bb(hdr_format, "packet_len")
+        self.digital_gmsk_mod_0 = digital.gmsk_mod(
+            samples_per_symbol=2,
+            bt=0.35,
+            verbose=False,
+            log=False,
+            do_unpack=True)
+        self.digital_crc32_bb_0 = digital.crc32_bb(False, "packet_len", True)
+        self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'packet_len', 0)
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 252, "packet_len")
+        self.blocks_repack_bits_bb_1 = blocks.repack_bits_bb(8, 1, "packet_len", False, gr.GR_MSB_FIRST)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(tx_gain)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/ahmad/GitHub/video-over-dsa/Transmitter/video1.ts', True, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.bladeRF_sink_0 = bladeRF.sink(
+            args="numchan=" + str(1)
+                 + ",metadata=" + 'False'
+                 + ",bladerf=" +  str('0')
+                 + ",verbosity=" + 'verbose'
+                 + ",fpga=" + str('')
+                 + ",fpga-reload=" + 'False'
+                 + ",use_ref_clk=" + 'False'
+                 + ",ref_clk=" + str(int(10e6))
+                 + ",in_clk=" + 'ONBOARD'
+                 + ",out_clk=" + str(False)
+                 + ",use_dac=" + 'False'
+                 + ",dac=" + str(10000)
+                 + ",xb200=" + 'none'
+                 + ",tamer=" + 'internal'
+                 + ",sampling=" + 'internal'
+                 + ",lpf_mode="+'disabled'
+                 + ",smb="+str(int(0))
+                 + ",dc_calibration="+'LPF_TUNING'
+                 + ",trigger0="+'False'
+                 + ",trigger_role0="+'master'
+                 + ",trigger_signal0="+'J51_1'
+                 + ",trigger1="+'False'
+                 + ",trigger_role1="+'master'
+                 + ",trigger_signal1="+'J51_1'
+                 + ",bias_tee0="+'True'
+                 + ",bias_tee1="+'False'
+
+
+        )
+        self.bladeRF_sink_0.set_sample_rate(samp_rate)
+        self.bladeRF_sink_0.set_center_freq(2520e6,0)
+        self.bladeRF_sink_0.set_bandwidth(6e6,0)
+        self.bladeRF_sink_0.set_gain(10, 0)
+        self.bladeRF_sink_0.set_if_gain(20, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.bladeRF_sink_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.blocks_repack_bits_bb_1, 0), (self.digital_gmsk_mod_0, 0))
+        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_crc32_bb_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_repack_bits_bb_1, 0))
+        self.connect((self.digital_crc32_bb_0, 0), (self.blocks_tagged_stream_mux_0, 1))
+        self.connect((self.digital_crc32_bb_0, 0), (self.digital_protocol_formatter_bb_0, 0))
+        self.connect((self.digital_gmsk_mod_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.digital_protocol_formatter_bb_0, 0), (self.blocks_tagged_stream_mux_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "transmitter")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_num_key(self):
+        return self.num_key
+
+    def set_num_key(self, num_key):
+        self.num_key = num_key
+        self.set_hdr_format(digital.header_format_crc(self.len_key, self.num_key))
+
+    def get_len_key(self):
+        return self.len_key
+
+    def set_len_key(self, len_key):
+        self.len_key = len_key
+        self.set_hdr_format(digital.header_format_crc(self.len_key, self.num_key))
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.blocks_multiply_const_vxx_0.set_k(self.tx_gain)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.bladeRF_sink_0.set_sample_rate(self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+
+    def get_hdr_format(self):
+        return self.hdr_format
+
+    def set_hdr_format(self, hdr_format):
+        self.hdr_format = hdr_format
+
+
+
+
+def main(top_block_cls=transmitter, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
